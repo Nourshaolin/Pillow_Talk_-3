87d99dd23a9febc9261c78e4e8953f2041b372fd
Nourshaolin <N.ElMoujahid@aui.ma>
2025-07-02 09:30:08 +0100

    commit 1

--- src/main/java/pillow/system/controller/User.java
+++ src/main/java/pillow/system/controller/User.java
@@ -1,10 +1,12 @@
 package pillow.system.controller;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="User")
 public class User {
-
+    @Id
+    @
 }
